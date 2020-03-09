4952924328c35480d5bd5c5d432f6f907743e114
Jeet Sangani <jeetsangani@gmail.com>
2020-03-09 06:44:31 -0400

    modal

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@ window.onload = function () {
     let glowRight = second.getElementById('glowRight');
     let glowLeft = second.getElementById('glowLeft');
     let glowPerson = second.getElementById('glowPerson');
+    let mainPerson = second.getElementById('person');
     let myth1Title = second.getElementById('title');
     let table = second.getElementById('table');
     let cageLeft = second.getElementById('cageLeft');
@@ -74,11 +75,11 @@ window.onload = function () {
 
 
     // code to make first slide fade in
-    function reset(){
-        TweenMax.set([myth1Title,myth2Title,table, cageLeft,person, cageRight,glowRight, glowLeft, glowPerson, glowJacket, glowClipboard, glowChart, glowMatress, glowNeedle, glowBottle, glowBottleEnd, beakers, nextSteps, info],{
+    function reset() {
+        TweenMax.set([myth1Title, myth2Title, table, cageLeft, person, cageRight, glowRight, glowLeft, glowPerson, glowJacket, glowClipboard, glowChart, glowMatress, glowNeedle, glowBottle, glowBottleEnd, beakers, nextSteps, info], {
             clearProps: "all"
         })
-        }
+    }
 
     // functions to make items fade in or appear
     function fadeInScreen1(clicked) {
@@ -559,21 +560,36 @@ window.onload = function () {
     document.getElementById('fadein5').onclick = fadeInScreen5;
     // document.getElementById('fadeout5').onclick = fadeOutScreen5;
 
-    // function checkPageFocus() {
 
-    //     // Targeting div for checking focus
-    //     let firstFocus = document.querySelector('#firstFocus') 
-    //     let check = document.activeElement.classList;
-        
-    //     if (check) {
-    //         firstFocus.classList.add("addCss");
-    //     }
-    //     else{
-    //         firstFocus.classList.remove("addCss");
-    //     }
-    //     console.log(check);
-    // }
 
-    // setInterval(checkPageFocus, 300);
+    /*********************** modal ************************/
+    
+
+    /*********************** page 1 ************************/
+    // Get the modal
+    let modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    
+
+    // Get the <span> element that closes the modal
+    let span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    mainPerson.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 
 }
