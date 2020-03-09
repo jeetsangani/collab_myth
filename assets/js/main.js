f29adf0e4cd1f3b19110f1866367c7d678ebcdf3
Jeet Sangani <jeetsangani@gmail.com>
2020-03-09 03:14:40 -0400

    tu

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@
         for (var i = 0; i < timeline.dateValues.length; i++) {
             var j = (i == 0) ? 0 : i + 0 ;
             var distance = daydiff(timeline.dateValues[j], timeline.dateValues[i]),
-                distanceNorm = (Math.round(distance / timeline.minLapse) + 1.3) * timeline.eventsMinDistance;
+                distanceNorm = (Math.round(distance / timeline.minLapse) + 1.1) * timeline.eventsMinDistance;
 
             if (distanceNorm < timeline.eventsMinDistance) {
                 distanceNorm = timeline.eventsMinDistance;
